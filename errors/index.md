47384bf46f3f5409a59074e291dc4769edbb92f5
Ivan-blade <2734319366@qq.com>
2020-05-08 17:08:13 +0800

    update

--- errors/index.md
+++ errors/index.md
@@ -4,7 +4,11 @@
 + 引用类型变量用于存放对象的地址，可以给引用类型赋值为null，表示不指向任何对象
 + 当某个引用类型变量为null时无法对对象实施访问，此时如果通过引用访问成员变量或调用方法会产生NullPointerException
 #### 算术异常
+
 #### 数组下标越界
 
 #### 数据类型转换异常
 + classCastException
+
+#### 数字格式异常（数字中含有字母）
++ NumberFormatException
--- temp/DogTest.java
+++ temp/DogTest.java
@@ -5,9 +5,7 @@ public class DogTest {
 
     public static void main(String[] args) {
 
-        Dog dog1 = new Dog();
-        dog1.show();
-        Dog dog2 = new Dog("luna","blue",18);
-        dog2.show();
+        int res = Integer.parseInt("0",16)+1;
+        System.out.println(res);
     }
 }
